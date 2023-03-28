9197f491f1a14d3d43fe67dbceeade0435a7e710
denefi <denefi@posteo.de>
2023-03-28 13:08:25 +0200

    get list of user articles from dbquery

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,16 +2,27 @@ const express = require("express");
 const router = express.Router();
 const Article = require("../models/article.model");
 const User = require("../models/User.model");
+const mongoose = require("mongoose");
 
 router.get("/:userId", (req, res, next) => {
-  res.render("../views/user/user-page");
   const userId = req.params.userId;
-  console.log(userId);
+  const user = {};
   User.findById(userId)
     .then((userFromDb) => {
       if (userFromDb) {
-        return Article.find();
+        user.name = userFromDb.username;
+        user.id = userFromDb.id;
+        return Article.find({ author: user.id }).populate("author");
       }
+    })
+    .then((articlesFromDb) => {
+      // const articles = articlesFromDb.filter((article) => {
+      //   if (article.author.id === user.id) {
+      //     return article;
+      //   }
+      // });
+      const articles = articlesFromDb;
+      res.render("user/user-page", { articles });
     })
     .catch((err) => {
       console.error(err);
