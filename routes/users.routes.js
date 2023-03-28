a43a77402ca3eb1b66578fe16c80616ff4a930bf
denefi <denefi@posteo.de>
2023-03-28 09:34:35 +0200

    add get route for user, check if user exists

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,3 +2,18 @@ const express = require("express");
 const router = express.Router();
 const Article = require("../models/article.model");
 const User = require("../models/User.model");
+
+router.get("/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  console.log(userId);
+  User.findById(userId)
+    .then((userFromDb) => {
+      if (userFromDb) {
+        return Article.find();
+      }
+      res.status(500).render("error", { message: "User not found" });
+    })
+    .catch((err) => next(err));
+});
+
+module.exports = router;
