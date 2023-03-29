9910a2c9a94cf651065a0dfa297be10dc4870cea
denefi <denefi@posteo.de>
2023-03-29 20:17:39 +0200

    feat/views-based-on-owner-status (#12)
    * cards aligned title centered
    
    * more css added background positioned cards
    
    * home page cards edited again
    
    * Add editDelete button partial
    
    * add id and owner status to article page
    
    * redirect users to their page after login
    
    * new articlelist function
    takes as arguments req and optionally a user id
    returns a sorted array of articles
    
    * use new article list method in routes
    
    * add edit delete buttons to views
    
    * fix images on index
    
    * add link to home at navbar
    
    * fix: Fix broken See Full Article button
    
    * fix signup
    
    * fix edit delete for all contextes
    
    * add link to author on index
    
    * fix author link on index
    
    * make userpage similar to index and add
    
    ---------
    
    Co-authored-by: drose123456 <acg_danial@hotmail.com>

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,40 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const Article = require("../models/article.model");
-const User = require("../models/User.model");
-const mongoose = require("mongoose");
+const listArticles = require("../utils/articleList");
 
 router.get("/:userId", (req, res, next) => {
   const userId = req.params.userId;
-  const user = {};
-  User.findById(userId)
-    .then((userFromDb) => {
-      if (userFromDb) {
-        user.name = userFromDb.username;
-        user.id = userFromDb.id;
-        return Article.find({ author: user.id }).populate("author");
-      }
-    })
-    .then((articlesFromDb) => {
-      const articles = articlesFromDb.map((article) => {
-        return (article = {
-          content: article.content,
-          id: article.id,
-          title: article.title,
-          author: article.author.username,
-          authorId: article.author._id,
-          creationMonth: article.createdAt.getMonth(),
-          creationYear: article.createdAt.getFullYear(),
-          img: article.imgUrl,
-        });
-      });
+  listArticles(req, userId)
+    .then((articles) => {
       res.render("user/user-page", { articles });
     })
-    .catch((err) => {
-      console.error(err);
-      // res.status(500).render("error", { message: "User not found" });
-      res.status(500).render("error", { message: "User Not Found" });
-    });
+    .catch((err) => next(err));
 });
 
 module.exports = router;
---
+++ utils/articleList.js
@@ -0,0 +1,51 @@
+const Article = require("../models/article.model");
+const User = require("../models/User.model");
+const isOwner = require("../utils/isOwner");
+
+async function checkUser(userId) {
+  console.log("trying for user");
+  try {
+    userFromDb = await User.findById(userId);
+    return userFromDb.id;
+  } catch (err) {
+    console.error(err);
+    // res.status(499).render("error", { message: "User not found" });
+    res.status(499).render("error", { message: "User Not Found" });
+  }
+}
+
+async function listArticles(req, searchForUserById) {
+  try {
+    console.log("argument passed to list Articles" + searchForUserById);
+    const query = {};
+    if (searchForUserById) {
+      const checkedUserId = await checkUser(searchForUserById);
+      console.log("checked user Id: " + checkedUserId);
+      query.author = checkedUserId;
+    }
+    articlesFromDb = await Article.find(query).populate("author");
+    articlesFromDb.sort((a, b) => {
+      return b.createdAt - a.createdAt;
+    });
+
+    const articles = articlesFromDb.map((article) => {
+      const ownerStatus = isOwner(req, article.author.id);
+      return (article = {
+        content: article.content.slice(0, 200),
+        id: article.id,
+        title: article.title,
+        author: article.author.username,
+        authorId: article.author.id,
+        creationMonth: article.createdAt.getMonth(),
+        creationYear: article.createdAt.getFullYear(),
+        img: article.imgUrl,
+        owner: ownerStatus,
+      });
+    });
+    return articles;
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = listArticles;
