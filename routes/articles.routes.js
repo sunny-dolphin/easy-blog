5d683b197e922089b0d66eba7a3f6664fc992cd3
denefi <denefi@posteo.de>
2023-03-30 17:33:38 +0200

    Validation to user Input & cloudinary fix (#18)
    * add axios
    
    * add password check module
    
    * add password pawn check to signup
    
    * cloudinary fixed
    
    * validation complete
    
    * basic css on nav bar added. title changed
    
    * fix required in inputform
    
    * update pictures and validation on edit
    
    ---------
    
    Co-authored-by: drose123456 <acg_danial@hotmail.com>

--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -7,8 +7,7 @@ const Article = require("../models/article.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const isOwner = require("../utils/isOwner");
 const User = require("../models/User.model");
-const cloudinary = require("../middleware/cloudinaryMiddleware");
-const multer = require("../middleware/multerMiddleware");
+const fileUploader = require("../config/cloudinary.config");
 
 // Create an article
 router.get("/create", isLoggedIn, (req, res, next) => {
@@ -16,23 +15,48 @@ router.get("/create", isLoggedIn, (req, res, next) => {
 });
 // Store the data received from create article form into database
 
-router.post("/create", isLoggedIn, multer, cloudinary, (req, res, next) => {
-  const blog = {
-    title: req.body.title,
-    author: req.session.currentUser._id,
-    topics: req.body.topics,
-    content: req.body.content,
-    imgUrl: req.body.imgUrl,
-  };
-  console.log(blog);
+router.post(
+  "/create",
+  isLoggedIn,
+  fileUploader.single("imgPath"),
+  (req, res, next) => {
+    console.log(req.file.path);
+    const blog = {
+      title: req.body.title,
+      author: req.session.currentUser._id,
+      topics: req.body.topics,
+      content: req.body.content,
+      imgUrl: req.file.path,
+    };
+    console.log(blog);
 
-  Article.create(blog)
-    .then((newArticle) => {
-      // console.log(newArticle);
-      res.redirect(`/articles/${newArticle.id}`);
-    })
-    .catch((err) => next(err));
-});
+    // Regular expression to check if title has only characters and spaces.
+    const regex = /^[a-zA-Z\s]+$/;
+    if (!regex.test(req.body.title)) {
+      return res.status(400).render("create-article", {
+        errorMessage:
+          "You title should only contain words, no special characters or numbers allowed",
+      });
+    }
+    if (
+      req.body.title == "" ||
+      req.body.imgUrl == "" ||
+      req.body.content == ""
+    ) {
+      return res.status(400).render("create-article", {
+        errorMessage:
+          "All the fields are mandatory, Please provide title, description and select an image",
+      });
+    }
+
+    Article.create(blog)
+      .then((newArticle) => {
+        // console.log(newArticle);
+        res.redirect(`/articles/${newArticle.id}`);
+      })
+      .catch((err) => next(err));
+  }
+);
 
 router.get("/:id", (req, res, next) => {
   const articleId = req.params.id;
@@ -75,26 +99,64 @@ router.get("/:id/edit", (req, res, next) => {
     });
 });
 
-router.post("/:id/edit", (req, res, next) => {
-  const { id } = req.params;
-  Article.findById(id)
-    .populate("author")
-    .then((articleToEdit) => {
-      if (isOwner(req, articleToEdit.author.id)) {
-        return Article.findByIdAndUpdate(req.params.id, req.body, {
-          new: true,
-        });
-      } else {
-        console.log("Unauthorized");
-        res.redirect(401, "/");
-      }
-    })
-    .then((article) => res.redirect(`/articles/${article.id}`))
-    .catch((e) => {
-      console.log("error updating article", e);
-      next(e);
-    });
-});
+router.post(
+  "/:id/edit",
+  isLoggedIn,
+  fileUploader.single("imgPath"),
+  (req, res, next) => {
+    console.log(req.body, req.file);
+    const { id } = req.params;
+    let imageUrl;
+    if (req.file) {
+      console.log("New File");
+      imageUrl = req.file.path;
+    } else {
+      console.log("No new image");
+      imageUrl = req.body.existingImage;
+    }
+    const blog = {
+      title: req.body.title,
+      author: req.session.currentUser._id,
+      topics: req.body.topics,
+      content: req.body.content,
+      imgUrl: imageUrl,
+    };
+    const regex = /^[a-zA-Z\s]+$/;
+    if (!regex.test(req.body.title)) {
+      return res.status(400).render("create-article", {
+        errorMessage:
+          "You title should only contain words, no special characters or numbers allowed",
+      });
+    }
+    if (
+      req.body.title == "" ||
+      req.body.imgUrl == "" ||
+      req.body.content == ""
+    ) {
+      return res.status(400).render("create-article", {
+        errorMessage:
+          "All the fields are mandatory, Please provide title, description and select an image",
+      });
+    }
+    Article.findById(id)
+      .populate("author")
+      .then((articleToEdit) => {
+        if (isOwner(req, articleToEdit.author.id)) {
+          return Article.findByIdAndUpdate(req.params.id, blog, {
+            new: true,
+          });
+        } else {
+          console.log("Unauthorized");
+          res.redirect(401, "/");
+        }
+      })
+      .then((article) => res.redirect(`/articles/${article.id}`))
+      .catch((e) => {
+        console.log("error updating article", e);
+        next(e);
+      });
+  }
+);
 
 router.get("/:id/delete", (req, res, next) => {
   const { id } = req.params;
