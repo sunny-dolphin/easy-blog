96b0f388f93aeea4d99b61a0d9732698a43197db
drose123456 <acg_danial@hotmail.com>
2023-03-30 12:02:05 +0100

    cloudinary fixed

--- middleware/multerMiddleware.js
+++
@@ -1,7 +0,0 @@
-const multer = require("multer");
-
-// Set up Multer to handle file uploads
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-module.exports = upload.single("imgPath");
